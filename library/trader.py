6ff368b78254f56183553dc49f8894d46e47183a
Erik Meulman <realtwister@gmail.com>
2019-05-11 23:11:25 +0200

    add initial library

---
+++ library/trader.py
@@ -0,0 +1,20 @@
+from server_interface import ServerInterface
+import numpy as np
+import time
+interface = ServerInterface()
+name = "cookie2"
+
+def callback(entry):
+    if entry['TYPE'] == 'TRADE':
+        feedcode = "ESX-FUTURE"
+        if entry['FEEDCODE'] == 'ESX-FUTURE':
+            feedcode = "SP-FUTURE"
+
+        if entry['SIDE'] == 'BID':
+            print("selling")
+            interface.sell(name, feedcode,  1000,np.ceil(int(entry['VOLUME'])/10).astype(int))
+        else:
+            print("buying")
+            interface.buy(name, feedcode, 10000, np.ceil(int(entry['VOLUME'])/10).astype(int))
+interface.register_callback(callback)
+interface.start_listen()
